73a86e2ef5d3ecf05131a07def241571c24f51e5
Nitish Rana <nitishrana@Nitishs-MacBook-Air.local>
2021-06-02 00:11:01 +0530

    Updates

---
+++ precache-manifest.bbbcef6b2f5a777cbab5ebf3fea1a996.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd6c5ba7d2f972fbb0fc2d921213187",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "5693703e7fe892af5b10",
+    "url": "/Keeper/static/js/2.ff4ff6e6.chunk.js"
+  },
+  {
+    "revision": "4d67d526ea97387b08e3",
+    "url": "/Keeper/static/js/main.dc5599a5.chunk.js"
+  },
+  {
+    "revision": "2e171cf057d5a30d7c8f",
+    "url": "/Keeper/static/js/runtime-main.cbd830f6.js"
+  }
+]);
